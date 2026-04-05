00658ebd1ac78888304ff316ce5e2aa50f1377b3
ASHALAN777 <cocguys777@gmail.com>
2026-04-05 15:25:59 +0530

    uhjj

--- Server/Routes/apiroutes.js
+++ Server/Routes/apiroutes.js
@@ -1,41 +1,68 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //import files//
 
-const { getAllJobs, getJobById, createJob, updateJob, deleteJob,getPublicJobs } = require("../controllers/Jobcontroller")
-const { applyJob, getAllApplications, getMyApplications, updateStatus,getApplicationById } = require("../controllers/applicationcontroller")
-const { scoreResume } = require("../controllers/aicontroller")
-const { uploadResume } = require("../controllers/uploadcontroller")
+const {
+  getAllJobs,
+  getJobById,
+  createJob,
+  updateJob,
+  deleteJob,
+  getPublicJobs,
+} = require("../controllers/Jobcontroller");
+const {
+  applyJob,
+  getAllApplications,
+  getMyApplications,
+  updateStatus,
+  getApplicationById,
+} = require("../controllers/applicationcontroller");
+const { scoreResume } = require("../controllers/aicontroller");
+const { uploadResume } = require("../controllers/uploadcontroller");
 
-const authMiddleware = require("../middleware/authMiddleware")
-const isAdmin = require("../middleware/rolemiddleware")
-const upload = require("../middleware/uploadmiddleware")
-const { createJobValidation, updateJobValidation, applyJobValidation } = require("../middleware/AuthValidation")
+const authMiddleware = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/rolemiddleware");
+const upload = require("../middleware/uploadmiddleware");
+const {
+  createJobValidation,
+  updateJobValidation,
+  applyJobValidation,
+} = require("../middleware/AuthValidation");
 
 // importtt done
 
 // Jobs
-router.get("/jobs/public", getPublicJobs)    // public, no auth
-router.get("/jobs", authMiddleware, isAdmin, getAllJobs )   // admin,  auth
-router.get("/jobs/:id", getJobById)   // public, no auth
-router.post("/jobs", authMiddleware, isAdmin, createJobValidation, createJob)
-router.put("/jobs/:id", authMiddleware, isAdmin, updateJobValidation, updateJob)
-router.delete("/jobs/:id", authMiddleware, isAdmin, deleteJob)
+router.get("/jobs/public", getPublicJobs); // public, no auth
+router.get("/jobs", authMiddleware, isAdmin, getAllJobs); // admin,  auth
+router.get("/jobs/:id", getJobById); // public, no auth
+router.post("/jobs", authMiddleware, isAdmin, createJobValidation, createJob);
+router.put(
+  "/jobs/:id",
+  authMiddleware,
+  isAdmin,
+  updateJobValidation,
+  updateJob,
+);
+router.delete("/jobs/:id", authMiddleware, isAdmin, deleteJob);
 
 // Applications
-router.post("/applications", authMiddleware, applyJobValidation,applyJob)
-router.get("/applications", authMiddleware, isAdmin, getAllApplications) //shw application
+router.post("/applications", authMiddleware, applyJobValidation, applyJob);
+router.get("/applications", authMiddleware, isAdmin, getAllApplications); //shw application
 
-router.get("/applications/:id", authMiddleware, isAdmin, getApplicationById)
-router.get("/applications/me", authMiddleware, getMyApplications)
-router.put("/applications/:id/status", authMiddleware, isAdmin, updateStatus)
-
+router.get("/applications/me", authMiddleware, getMyApplications);
+router.get("/applications/:id", authMiddleware, isAdmin, getApplicationById);
+router.put("/applications/:id/status", authMiddleware, isAdmin, updateStatus);
 
 // AI
-router.post("/ai/score", authMiddleware, scoreResume)
+router.post("/ai/score", authMiddleware, scoreResume);
 
 // Upload
-router.post("/upload/resume", authMiddleware, upload.single("resume"), uploadResume)
+router.post(
+  "/upload/resume",
+  authMiddleware,
+  upload.single("resume"),
+  uploadResume,
+);
 
-module.exports = router
+module.exports = router;
